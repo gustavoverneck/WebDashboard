6fb501e9aabf087d40f48097e16745b6ce6c277a
Guilherme de Melo <gdmelo.silva@gmail.com>
2024-12-19 00:17:03 -0300

    Ajustes-gui

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from src.routes import main
 
 def create_app():
     app = Flask(__name__)
-    window = webview.create_window('WebDashboard', app)
+    window = webview.create_window('WebDashboard', app, min_size=(1200, 800))
 
     # Importando e registrando o Blueprint
     app.register_blueprint(main)
